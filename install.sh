13638c20595340ffa064c5569ca35d48bcdc9721
Zeb Perkins <zebmperkins@gmail.com>
2025-04-27 16:46:30 -0400

    temp commit 7

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ python3 -m pip install --upgrade pip
 python3 -m pip install scons
 dnf -y install dotnet-sdk-9.0 python3
 export PATH="/usr/local/bin:$PATH"
+export GODOT_VERSION_STATUS="testing123"
 
 # Build c++
 rm -rf bin
@@ -31,15 +32,15 @@ scons platform=linuxbsd target=editor tools=yes module_mono_enabled=yes mono_glu
 scons platform=linuxbsd target=template_debug module_mono_enabled=yes
 scons platform=linuxbsd target=template_release module_mono_enabled=yes
 
-# Generate glue
-./bin/godot.linuxbsd.editor.x86_64.mono --headless --generate-mono-glue ./modules/mono/glue
-
 # Install this version of the killer carrot godot fork as a nuget package
 dotnet nuget locals all --clear
 rm -rf ~/MyLocalNugetSource
 mkdir ~/MyLocalNugetSource
 dotnet nuget remove source MyLocalNugetSource
 dotnet nuget add source ~/MyLocalNugetSource --name MyLocalNugetSource
+
+# Generate glue
+./bin/godot.linuxbsd.editor.x86_64.mono --headless --generate-mono-glue ./modules/mono/glue
 
 # Generate binaries
 python3 modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --push-nupkgs-local ~/MyLocalNugetSource --godot-platform=linuxbsd
