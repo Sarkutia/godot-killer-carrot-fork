84556f50f69dbb7fe825c4ef05ccf13cab2b7e4e
Zeb Perkins <zebmperkins@gmail.com>
2025-04-27 14:58:52 -0400

    temp commit 5

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp -r /godot-src/bin/. /mnt/bin
-cp -r /godot-src/modules/mono/glue/. /mnt/glue
--- install.sh
+++ install.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
-# install dependencies for local godot development on this machine
+# Install dependencies
+dnf install -y sudo
+sudo dnf update -y
+sudo dnf install -y \
+  pkgconfig \
+  gcc-c++ \
+  libstdc++-static \
+  wayland-devel \
+  wget
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 ./get-pip.py
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install scons
 sudo dnf -y install dotnet-sdk-9.0 python3
+export PATH="/usr/local/bin:$PATH"
 
-# create a build of killer carrot godot fork from this source
-podman build --tag godotbuild .
-
-# run the container
+# Build c++
 sudo rm -rf bin
 mkdir bin
 sudo chmod 777 bin
+scons platform=linuxbsd target=editor tools=yes module_mono_enabled=yes mono_glue=yes
+
+# Build export templates
+scons platform=linuxbsd target=template_debug module_mono_enabled=yes
+scons platform=linuxbsd target=template_release module_mono_enabled=yes
+
+# Generate glue
 sudo rm -rf modules/mono/glue
 mkdir -p modules/mono/glue
 sudo chmod 777 modules/mono/glue
-podman run --rm -v $(pwd)/bin:/mnt/bin -v $(pwd)/modules/mono/glue:/mnt/glue godotbuild:latest
+./bin/godot.linuxbsd.editor.x86_64.mono --headless --generate-mono-glue ./modules/mono/glue
 
-# install this version of the killer carrot godot fork as a nuget package
+# Install this version of the killer carrot godot fork as a nuget package
 dotnet nuget locals all --clear
 sudo rm -rf ~/MyLocalNugetSource
 mkdir ~/MyLocalNugetSource
+dotnet nuget remove source MyLocalNugetSource
 dotnet nuget add source ~/MyLocalNugetSource --name MyLocalNugetSource
 
 # Generate binaries
