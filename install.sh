718ec73d46caf057a0f01fa4f4c112dc0e8c5246
Zeb Perkins <zebmperkins@gmail.com>
2025-04-27 15:55:27 -0400

    temp commit 6

--- install.sh
+++ install.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# Ensure this script is run as root
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "This script needs to be run as root. Exiting..."
+    exit
+fi
+
 # Install dependencies
-dnf install -y sudo
-sudo dnf update -y
-sudo dnf install -y \
+dnf update -y
+dnf install -y \
   pkgconfig \
   gcc-c++ \
   libstdc++-static \
   wayland-devel \
   wget
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 ./get-pip.py
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install scons
-sudo dnf -y install dotnet-sdk-9.0 python3
+# wget https://bootstrap.pypa.io/get-pip.py
+# python3 ./get-pip.py
+python3 -m pip install --upgrade pip
+python3 -m pip install scons
+dnf -y install dotnet-sdk-9.0 python3
 export PATH="/usr/local/bin:$PATH"
 
 # Build c++
-sudo rm -rf bin
+rm -rf bin
 mkdir bin
-sudo chmod 777 bin
+chmod 777 bin
 scons platform=linuxbsd target=editor tools=yes module_mono_enabled=yes mono_glue=yes
 
 # Build export templates
@@ -27,21 +32,18 @@ scons platform=linuxbsd target=template_debug module_mono_enabled=yes
 scons platform=linuxbsd target=template_release module_mono_enabled=yes
 
 # Generate glue
-sudo rm -rf modules/mono/glue
-mkdir -p modules/mono/glue
-sudo chmod 777 modules/mono/glue
 ./bin/godot.linuxbsd.editor.x86_64.mono --headless --generate-mono-glue ./modules/mono/glue
 
 # Install this version of the killer carrot godot fork as a nuget package
 dotnet nuget locals all --clear
-sudo rm -rf ~/MyLocalNugetSource
+rm -rf ~/MyLocalNugetSource
 mkdir ~/MyLocalNugetSource
 dotnet nuget remove source MyLocalNugetSource
 dotnet nuget add source ~/MyLocalNugetSource --name MyLocalNugetSource
 
 # Generate binaries
-sudo python3 modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --push-nupkgs-local ~/MyLocalNugetSource --godot-platform=linuxbsd
+python3 modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --push-nupkgs-local ~/MyLocalNugetSource --godot-platform=linuxbsd
 
 # Add godot to path
 echo "$(pwd)/bin/godot.linuxbsd.editor.x86_64.mono" | sudo tee /usr/bin/godot > /dev/null
-sudo chmod +x /usr/bin/godot
+chmod +x /usr/bin/godot
